134af3593ef3a6fe2276d7b317f118bfd38e1d70
TXHENG <txh0607@gmail.com>
2022-08-07 01:35:34 +0800

    Design for donor screen

--- app/src/main/java/com/example/unseenfamily/ui/donation_history/DonationHistoryViewModel.kt
+++ app/src/main/java/com/example/unseenfamily/ui/donation_history/DonationHistoryViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.unseenfamily.ui.gallery
+package com.example.unseenfamily.ui.donation_history
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class DonationHistoryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
--- app/src/main/java/com/example/unseenfamily/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/unseenfamily/ui/home/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.example.unseenfamily.ui.home
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.forEach
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.unseenfamily.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,11 +31,16 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.scrollViewLinearLayout.forEach {
+            it.setOnClickListener {
+                findNavController().navigate(com.example.unseenfamily.R.id.action_nav_home_to_donationDetailsFragment)
+            }
+        }
     }
 
     override fun onDestroyView() {
